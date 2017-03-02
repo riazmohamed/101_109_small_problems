56ea7c81dfbe045f35002962c745bf6aea6a5be9
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-02 05:09:58 +0000

    repeat_yourself.rb

--- easy_1/repeat_yourself.rb
+++ easy_1/repeat_yourself.rb
@@ -1,1 +1,22 @@
-# repeat_yourself.rb 
+# repeat_yourself.rb
+
+# option 1
+def repeat(word, iteration)
+  iteration.times { |i| puts word }
+end
+
+repeat('Hello', 3)
+
+puts "============="
+# option 2
+
+def repeat(word, iteration)
+  counter = 0
+  loop do
+    break if counter == iteration
+    puts word
+    counter += 1
+  end
+end
+
+repeat('Hello', 3)
