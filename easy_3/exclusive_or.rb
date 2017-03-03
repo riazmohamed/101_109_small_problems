7c19992fe4da0b8b4b892b70b4128f42e41ab6dd
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-03 10:45:19 +0000

    add exclusive_or.rb

---
+++ easy_3/exclusive_or.rb
@@ -0,0 +1,11 @@
+# exclusive_or.rb
+
+def xor?(arg1, arg2)
+  arg1 || arg2
+end
+
+# example
+p xor?(5.even?, 4.even?) == true
+p xor?(5.odd?, 4.odd?) == true
+p xor?(5.odd?, 4.even?) == false
+p xor?(5.even?, 4.odd?) == false
