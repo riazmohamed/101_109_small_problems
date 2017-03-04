362a2f040572b04f583149d9554c4bda20d6b47c
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-04 22:11:13 +0000

    add solution for leap_year.rb

---
+++ easy_4/leap_year.rb
@@ -0,0 +1,62 @@
+# leap_year.rb
+
+def leap_year?(year)
+  if year % 400 == 0
+    true
+  elsif year % 100 == 0 && year % 400 == 0
+    true
+  elsif year % 4 == 0 && year % 100 != 0
+    true
+  else
+    false
+  end
+end
+
+p leap_year?(2016)
+p leap_year?(2015)
+p leap_year?(2100)
+p leap_year?(2400)
+p leap_year?(240000)
+p leap_year?(240001)
+p leap_year?(2000)
+p leap_year?(1900)
+p leap_year?(1752)
+p leap_year?(1700)
+p leap_year?(1)
+p leap_year?(100)
+p leap_year?(400)
+
+# expected answer
+
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == false
+# leap_year?(1) == false
+# leap_year?(100) == false
+# leap_year?(400) == true
+
+=begin
+In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
+
+Assume this rule is good for any year greater than year 0. Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
+=end
+
+puts "============"
+# given solution
+
+def leap_year?(year)
+  if year % 400 == 0
+    true
+  elsif year % 100 == 0
+    false
+  else
+    year % 4 == 0
+  end
+end
