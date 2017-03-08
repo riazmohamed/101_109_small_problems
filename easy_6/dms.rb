57821a82190252001edc7e0a5ec06ba63e901b5f
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-08 18:14:52 +0000

    add dms.rb

--- easy_6/dms.rb
+++ easy_6/dms.rb
@@ -1,16 +1,24 @@
 # dms.rb
 
-def dms(input)
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
+def dms(degrees_float)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
 end
 
 # Write a method that takes a floating point number that represents an angle between 0 and 360 degrees and returns a String that represents that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
 #
 # Examples:
 #
-# dms(30) == %(30°00'00")
-# dms(76.73) == %(76°43'48")
-# dms(254.6) == %(254°36'00")
-# dms(93.034773) == %(93°02'05")
-# dms(0) == %(0°00'00")
-# dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+p dms(30)
+p dms(76.73)
+p dms(254.6)
+p dms(93.034773)
+p dms(0)
+p dms(360)
