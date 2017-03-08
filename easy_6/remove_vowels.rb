b0d79fc9c4e62e7301d4cd65e8d149092ed04d28
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-08 18:52:48 +0000

    add given solution

--- easy_6/remove_vowels.rb
+++ easy_6/remove_vowels.rb
@@ -15,3 +15,9 @@ p remove_vowels(%w(ABC AEIOU XYZ))
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+# given solution
+
+def remove_vowels(arr)
+  arr.map { |word| word.delete('aeiouAEIOU') }
+end
