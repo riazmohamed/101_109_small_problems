edc7b203c8f706f2692c719c4d19dfe2c7ce74a8
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-09 14:57:51 +0000

    add given solution

--- easy_6/include?.rb
+++ easy_6/include?.rb
@@ -10,6 +10,12 @@ def include?(arr, element)
   arr.any? { |ele| ele == element }
 end
 
+# option 3
+def include?(arr, element)
+  arr.each { |ele| return true if ele == element }
+  false
+end
+
 p include?([1,2,3,4,5], 3)
 p include?([1,2,3,4,5], 6)
 p include?([], 3)
