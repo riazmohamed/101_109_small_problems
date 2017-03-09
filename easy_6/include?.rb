1b2bc1bcb254244045e428bbc47aff7f22a3e65d
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-09 14:55:50 +0000

    include?.rb

---
+++ easy_6/include?.rb
@@ -0,0 +1,17 @@
+# include?.rb
+
+# option 1
+def include?(arr, element)
+  !!arr.find_index(element)
+end
+
+# option 2
+def include?(arr, element)
+  arr.any? { |ele| ele == element }
+end
+
+p include?([1,2,3,4,5], 3)
+p include?([1,2,3,4,5], 6)
+p include?([], 3)
+p include?([nil], nil)
+p include?([], nil)
