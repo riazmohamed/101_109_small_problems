f774da9d9fd098b34d363dc39d2d73edc029fedf
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-11 13:30:30 +0000

    show_multiplicative_average.rb

---
+++ easy_7/show_multiplicative_average.rb
@@ -0,0 +1,13 @@
+# show_multiplicative_average.rb
+
+def show_multiplicative_average(arr)
+  result = 1.000
+  arr.each do |num|
+    result *= num
+  end
+  average = (result / arr.size)
+  puts "The result is #{format('%.3f', average)}"
+end
+
+p show_multiplicative_average([3, 5])
+p show_multiplicative_average([2, 5, 7, 11, 13, 17])
