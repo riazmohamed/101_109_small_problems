a35ace6a819c19b0389de5225782d0b1232f969c
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-11 13:33:26 +0000

    add notes

--- easy_7/show_multiplicative_average.rb
+++ easy_7/show_multiplicative_average.rb
@@ -5,9 +5,12 @@ def show_multiplicative_average(arr)
   arr.each do |num|
     result *= num
   end
+
   average = (result / arr.size)
   puts "The result is #{format('%.3f', average)}"
 end
 
 p show_multiplicative_average([3, 5])
 p show_multiplicative_average([2, 5, 7, 11, 13, 17])
+
+# can also use Ennumenrable#inject
