c69e57dc2a07da068a9b1689bbf40d8f6c3716bb
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-12 07:21:54 +0000

    add the best solution

--- easy_8/substrings_at_start.rb
+++ easy_8/substrings_at_start.rb
@@ -11,3 +11,14 @@ end
 p substrings_at_start('abc') == ['a', 'ab', 'abc']
 p substrings_at_start('a') == ['a']
 p substrings_at_start('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+# best solution
+def substr(str)
+  str.size <= 1 ? [str] : [*substr(str.chop), str]
+end
+
+p substr('abc')
+p substr('a')
+p substr('xyzzy')
+
+# Recursively chopping the last character and the * unpacks the multidimensional array into one dimensional array.
