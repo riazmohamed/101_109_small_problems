fbfad197bbe3a8519cb414cfa8c14795e07a70a5
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-12 06:58:34 +0000

    Add best solution

--- easy_8/madlibs.rb
+++ easy_8/madlibs.rb
@@ -14,3 +14,15 @@ sentence_2 = "The #{adjective} #{noun} #{verb}s #{adverb} over the lazy dog."
 sentence_3 = "The #{noun} #{adverb} #{verb}s up #{adjective} Joe's turtle."
 
 puts [sentence_1, sentence_2, sentence_3].sample
+
+# best answer
+
+def madlibs1
+  noun, verb, adj, adv = %w(noun verb adjective adverb).map do |word_type|
+    puts "Please enter a#{'n' if word_type[/\Aa/]} #{word_type}:"
+    gets.chomp
+  end
+  puts format("Do you %s your %s %s %s? That's hilarious!", verb, adj, noun, adv)
+end
+
+puts madlibs1
