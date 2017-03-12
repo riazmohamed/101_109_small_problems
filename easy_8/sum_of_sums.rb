4e472c7da91a420d108705c5f950e149e9265287
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-12 06:40:40 +0000

    add given solution

--- easy_8/sum_of_sums.rb
+++ easy_8/sum_of_sums.rb
@@ -12,3 +12,17 @@ p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
 p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
 p sum_of_sums([4]) == 4
 p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+# given solution
+
+def sum_of_sums1(arr)
+  sum = 0
+  1.upto(arr.size) do |num|
+    sum += arr.slice(0, num).reduce(:+)
+  end
+  sum
+end
+p sum_of_sums1([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums1([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums1([4]) == 4
+p sum_of_sums1([1, 2, 3, 4, 5]) == 35
