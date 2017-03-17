92db960d4f756d1aa4f3237f72873b553a64206a
Riaz Mohamed <railsdevriaz@gmail.com>
2017-03-17 04:49:37 +0000

    madlibs_revisited.rb

---
+++ advanced_1/madlibs_revisited.rb
@@ -0,0 +1,15 @@
+# madlibs_revisited.rb
+
+ADJECTIVES = %w(quick lazy sleepy ugly)
+NOUNS = %w(fox dog head leg)
+VERBS = %w(jumps lifts bites licks)
+ADVERBS = %w(easily noisily excitedly)
+
+File.open('madlib.txt') do |file|
+  file.each do |line|
+    puts format(line, noun: NOUNS.sample,
+                      verb: VERBS.sample,
+                      adjective: ADJECTIVES.sample,
+                      adverb: ADVERBS.sample)
+  end
+end
